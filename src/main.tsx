e29df2ec483f9d8f1443e4bd3ca9a9f39b120b71
Alexandra Ligacheva <al.ligacheva@gmail.com>
2024-08-18 01:39:21 +0200

    feat: implement main and app components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App.tsx';
+import { ArticleList } from './components/article-list';
+import { PersonalizationSettings } from './components/personalization-settings';
+import { ErrorPage } from './components/error-page';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <ArticleList />,
+        },
+        {
+          path: "/personalize",
+          element: <PersonalizationSettings />,
+        },
+      ],
+    },
+  ],
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
